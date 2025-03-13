9d363e1bd96a7e7c3fa64f8ed9988a483d96691c
Adnaan-boi <adnaanboi@gmail.com>
2025-03-13 12:48:31 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    printf("my name is adnaan");
+    printf("\n my university is isbat");
+    printf("\n i stay in kampala");
+    */
+/*
+    int x = 20;
+    int y = 10;SS
+    int sum =x-y;
+    int a=x-y;
+    int b=x*y;
+
+    printf("\n the sum of x and y is %d",x,y,sum);
+    printf("\n the difference of %d and %d is %d",x,y,a);
+    printf("\n the product of %d and %d is %d",x,y,b);
+    */
+
+
+    /* int x=10, m=60, p;
+    printf("the value of x is %d and m is %d \n",x,m);
+    float h=9.8;
+    printf("the value of h is %.2 f",h);
+   */
+
+   //int x=47;
+   // float y=500.18;
+     //double z=200.1;
+
+     //printf ("the number is %d\n",x);
+    // printf("the value of y is %f",y);
+     //printf("the value of z is %1f",z);
+
+     //printf("the value of x, y and z is 47,500")
+
+     //int x=100;
+    // x=400;
+     //int b=70,c=40,d,e;
+
+
+     //printf("the value of x is %d",x);
+/*
+     char boi[ ]="adnaan is a university";
+     printf("my name is %s",boi);
+     const float num1 =3.14
+     num1=100;
+    */
+
+/*
+    declar the following variable and assign them values
+    integer,character, flooat,double and string print each on a new line
+
+
+    int x=100
+      printf("the value of x%d\n",x);
+
+    float  = 75.25;
+      printf("the value of c is %f"c);
+
+    char b= 'M';
+      printf("the value of b is %c"d);
+
+    double D=21.31503659874;
+      printf("the value of d is %.81f\n"d);
+
+    char E[]="i love isbat university";
+
+    int x=100, y=400;
+    printf("the sum of x and y is %d",(x+1000));
+
+*/
+
+    return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
